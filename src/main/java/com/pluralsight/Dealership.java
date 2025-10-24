2141350f8c0c4d704bde5cade50c71b4989b10cb
DanielSpezialetti <spezialettidaniel@gmail.com>
2025-10-24 11:06:03 -0400

    * created Dealership and Vehicle classes * created variables for each class and constructors * added methods to Dealership that will be used later

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,53 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name, address, phone;
+
+    ArrayList<Vehicle> inventory = new ArrayList<>();
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+
+    public void getVehiclesByPrice(min, max) : ArrayList<Vehicle> {
+
+    }
+
+    public void getVehiclesByMakeModel(make, model) : ArrayList<Vehicle> {
+
+    }
+
+    public void getVehiclesByYear(min, max) : ArrayList<Vehicle> {
+
+    }
+
+    public void getVehiclesByColor(color) : ArrayList<Vehicle> {
+
+    }
+
+    public void getVehiclesByMileage(min, max) : ArrayList<Vehicle> {
+
+    }
+
+    public void getVehiclesByVehicleType(vehicleType) : ArrayList<Vehicle> {
+
+    }
+
+    public void getAllVehicles(min, max) : ArrayList<Vehicle> {
+
+    }
+
+    public void addVehicle(vehicle) {
+
+    }
+
+    public void removeVehicle(vehicle) {
+
+    }
+
+
+}
---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private int vin, year, odometer;
+    private String make, model, vehicleType, color;
+    private double price;
+
+    public Vehicle(int vin, int year, String make, String model, String vehicleType, String color, int odometer, double price) {
+        this.vin = vin;
+        this.year = year;
+        this.make = make;
+        this.model = model;
+        this.vehicleType = vehicleType;
+        this.color = color;
+        this.odometer = odometer;
+        this.price = price;
+    }
+
+
+
+}

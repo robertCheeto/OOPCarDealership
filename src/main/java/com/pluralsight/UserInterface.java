f3e9558a7b77c855a47a3e446b019ae0312189aa
DanielSpezialetti <spezialettidaniel@gmail.com>
2025-10-24 14:04:59 -0400

    * updated Main class to become the UserInterface * added methods to class that will get code added to it later * I have no idea what is going on truth be told

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,7 +1,77 @@
 package com.pluralsight;
 
 public class UserInterface {
-    public static void main(String[] args) {
+
+
+//    public static void main(String[] args) {
+//
+//    }
+
+    private void init() {
+        DealershipFileManager dealership = new DealershipFileManager();
+        dealership.getDealership();
 
     }
+
+    public static void display() {
+
+        while (isRunning) {
+            boolean isRunning;
+
+            switch (userInput) {
+                case 1:
+                    System.out.println("Process 1");
+                    break;
+                case 2:
+                    System.out.println("Process 2");
+                    break;
+                case 0:
+                    System.out.println("Ending Program");
+                    isRunning = false;
+                default:
+                    System.out.println("Invalid Input");
+                    break;
+            }
+
+        }
+
+    }
+
+    public static void processGetByPriceRequest() {
+
+    }
+
+    public static void processGetByMakeModelRequest() {
+
+    }
+
+    public static void processGetByYearRequest() {
+
+    }
+
+    public static void processGetByColorRequest() {
+
+    }
+
+    public static void processGetByMileageRequest() {
+
+    }
+
+    public static void processGetByVehicleTypeRequest() {
+
+    }
+
+    public static void processGetByAllVehiclesRequest() {
+
+    }
+
+    public static void processAddVehicleRequest() {
+
+    }
+
+    public static void processRemoveVehicleRequest() {
+
+    }
+
+
 }

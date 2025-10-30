0914e02f2c684e27cf1398e5f12b516635f497ab
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-30 15:36:45 -0400

    * commented out saveDealership() and will complete this code later

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,13 +1,15 @@
 package com.pluralsight;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class DealershipFileManager {
-    ArrayList<Vehicle> inventory = new ArrayList<>();
+    static ArrayList<Vehicle> inventory = new ArrayList<>();
 
-    public Dealership getDealership() {
+    public static Dealership getDealership() {
         Dealership dealership = null;
 
         try {
@@ -48,8 +50,40 @@ public class DealershipFileManager {
     }
 
     public static void saveDealership(Dealership dealership) {
-        // need to write to inventory file
-        // get dealership name, get the inventory and overwrite file
+//        try {
+//            BufferedWriter bufWriter = new BufferedWriter(new FileWriter("src/main/resources/inventory.csv", true));
+//
+//            String input = "";
+//            String[] parsedList = input.split("\\|");
+//
+//            while ((bufWriter.write(dealership)) != null) {
+//
+//                if (parsedList.length > 3) {
+//                    int vin = Integer.parseInt(parsedList[0]);
+//                    int year = Integer.parseInt(parsedList[1]);
+//                    String make = parsedList[2];
+//                    String model = parsedList[3];
+//                    String vehicleType = parsedList[4];
+//                    String color = parsedList[5];
+//                    int odometer = Integer.parseInt(parsedList[6]);
+//                    double price = Double.parseDouble(parsedList[7]);
+//
+//                    inventory.add(new Vehicle(vin, year, make, model, vehicleType, color, odometer, price));
+//                }
+//                else {
+//                    String name = parsedList[0];
+//                    String address = parsedList[1];
+//                    String phoneNum = parsedList[2];
+//
+//                    dealership = new Dealership(name, address, phoneNum);
+//                }
+//
+//            }
+//            bufWriter.close();
+//
+//        } catch (Exception e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
 

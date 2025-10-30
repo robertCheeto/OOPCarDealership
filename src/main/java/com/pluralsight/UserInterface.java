8a9832bf13b107aeec56d010577b22bbfe7b777e
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-30 14:56:52 -0400

    * updated display() to allow user to select option from main menu * created menuDisplay() that will display those menu options to the user

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -16,6 +16,7 @@ public class UserInterface {
     // need to load those into the switch case
     public void display() {
         init();
+        menuDisplay();
         boolean isRunning = true;
 
         while (isRunning) {
@@ -23,22 +24,34 @@ public class UserInterface {
             keyboard.nextLine();
 
             switch (userInput) {
-                case 1:
-                    System.out.println("Process 1");
-                    break;
-                case 2:
-                    System.out.println("Process 2");
-                    break;
-                case 0:
-                    System.out.println("Ending Program");
-                    isRunning = false;
-                default:
-                    System.out.println("Invalid Input");
-                    break;
+                case 1 -> processGetByPriceRequest();
+                case 2 -> processGetByMakeModelRequest();
+                case 3 -> processGetByYearRequest();
+                case 4 -> processGetByColorRequest();
+                case 5 -> processGetByMileageRequest();
+                case 6 -> processGetByVehicleTypeRequest();
+                case 7 -> processAllVehicleRequest();
+                case 8 -> processAddVehicleRequest();
+                case 9 -> processRemoveVehicleRequest();
+                case 99 -> System.exit(0);
+                default -> System.out.println("Invalid input, please try again.");
             }
-
         }
+    }
 
+    public void menuDisplay() {
+        System.out.println("*** Select Option via Number Input ***");
+        System.out.println("1 - Find vehicles within a price range\n" +
+                "2 - Find vehicles by make / model\n" +
+                "3 - Find vehicles by year range\n" +
+                "4 - Find vehicles by color\n" +
+                "5 - Find vehicles by mileage range\n" +
+                "6 - Find vehicles by type (car, truck, SUV, van)\n" +
+                "7 - List ALL vehicles\n" +
+                "8 - Add a vehicle\n" +
+                "9 - Remove a vehicle\n" +
+                "99 - Quit");
+        System.out.print("Enter user choice here: ");
     }
 
     private void displayVehicles(Dealership dealership) {
@@ -92,6 +105,5 @@ public class UserInterface {
     public static void processRemoveVehicleRequest() {
 
     }
-
 
 }

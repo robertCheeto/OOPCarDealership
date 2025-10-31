aa3655367705b015a816b9b1cb8e122900d7ff13
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-31 11:35:04 -0400

    * created a Model folder and an abstract Contract Parent class * created Contract variables and Constructor * created contract getters and set getTotalPrice() and getMonthlyPayment() to be abstract

---
+++ src/main/java/com/pluralsight/Models/Contract.java
@@ -0,0 +1,35 @@
+package com.pluralsight.Models;
+
+public abstract class Contract {
+    private String contractDate, customerName, customerEmail, vehicleSold;
+    private double totalPrice, monthlyPayment;
+
+    public Contract(String contractDate, String customerName, String customerEmail, String vehicleSold, double totalPrice, double monthlyPayment) {
+        this.contractDate = contractDate;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+        this.totalPrice = totalPrice;
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    public String getContractDate() {
+        return contractDate;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public abstract getTotalPrice();
+
+    public abstract getMonthlyPayment();
+}

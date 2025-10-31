c4e29fdbf1a3940eda49ab55f72ccd69ddb5b438
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-31 13:18:16 -0400

    * need to make an if statement that determine whether or not an entry in the contracts.csv is a SALE or LEASE * updated code to reflect the change above

--- src/main/java/com/pluralsight/ContractManagement/ContractFileManager.java
+++ src/main/java/com/pluralsight/ContractManagement/ContractFileManager.java
@@ -17,28 +17,34 @@ public class ContractFileManager {
             while ((input = bufReader.readLine()) != null) {
                 String[] parsedList = input.split("\\|");
 
-                String contractType = parsedList[0];
-                String contractDate = parsedList[1];
-                String customerName = parsedList[2];
-                String customerEmail = parsedList[3];
-                int vin = Integer.parseInt(parsedList[4]);
-                int year = Integer.parseInt(parsedList[5]);
-                String make = parsedList[6];
-                String model = parsedList[7];
-                String vehicleType = parsedList[8];
-                String color = parsedList[9];
-                int mileage = Integer.parseInt(parsedList[10]);
-                double price = Double.parseDouble(parsedList[11]);
-                double salesTax = Double.parseDouble(parsedList[12]);
-                int recordingFee = Integer.parseInt(parsedList[13]);
-                int processingFee = Integer.parseInt(parsedList[14]);
-                double totalPrice = Double.parseDouble(parsedList[15]);
-                String isFinanced = parsedList[16];
-                double monthlyPayment = Double.parseDouble(parsedList[17]);
+                if (parsedList[0].equals("SALE")) {
+                    String contractType = parsedList[0];
+                    String contractDate = parsedList[1];
+                    String customerName = parsedList[2];
+                    String customerEmail = parsedList[3];
+                    int vin = Integer.parseInt(parsedList[4]);
+                    int year = Integer.parseInt(parsedList[5]);
+                    String make = parsedList[6];
+                    String model = parsedList[7];
+                    String vehicleType = parsedList[8];
+                    String color = parsedList[9];
+                    int mileage = Integer.parseInt(parsedList[10]);
+                    double price = Double.parseDouble(parsedList[11]);
+                    double salesTax = Double.parseDouble(parsedList[12]);
+                    int recordingFee = Integer.parseInt(parsedList[13]);
+                    int processingFee = Integer.parseInt(parsedList[14]);
+                    double totalPrice = Double.parseDouble(parsedList[15]);
+                    String isFinanced = parsedList[16];
+                    double monthlyPayment = Double.parseDouble(parsedList[17]);
 
-                salesContract = new SalesContract(contractType, contractDate, customerName, customerEmail,
-                        vin, year, make, model, vehicleType, color, mileage, price, salesTax, recordingFee,
-                        processingFee, totalPrice, isFinanced, monthlyPayment);
+                    salesContract = new SalesContract(contractType, contractDate, customerName, customerEmail,
+                            vin, year, make, model, vehicleType, color, mileage, price, salesTax, recordingFee,
+                            processingFee, totalPrice, isFinanced, monthlyPayment);
+                }
+                else {
+                    System.out.println("Whoops, no sales!");
+                }
+
             }
             bufReader.close();
 
@@ -49,6 +55,48 @@ public class ContractFileManager {
     }
 
     public static LeaseContract getLeaseContracts() {
+        LeaseContract leaseContract = null;
+
+        try {
+            BufferedReader bufReader = new BufferedReader(new FileReader("src/main/resources/contracts.csv"));
+
+            String input;
+
+            while ((input = bufReader.readLine()) != null) {
+                String[] parsedList = input.split("\\|");
+
+                if (parsedList[0].equals("LEASE")) {
+                    String contractType = parsedList[0];
+                    String contractDate = parsedList[1];
+                    String customerName = parsedList[2];
+                    String customerEmail = parsedList[3];
+                    int vin = Integer.parseInt(parsedList[4]);
+                    int year = Integer.parseInt(parsedList[5]);
+                    String make = parsedList[6];
+                    String model = parsedList[7];
+                    String vehicleType = parsedList[8];
+                    String color = parsedList[9];
+                    int mileage = Integer.parseInt(parsedList[10]);
+                    double price = Double.parseDouble(parsedList[11]);
+                    double expectedEndValue = Double.parseDouble(parsedList[12]);
+                    int leaseFee = Integer.parseInt(parsedList[13]);
+                    double totalPrice = Double.parseDouble(parsedList[15]);
+                    double monthlyPayment = Double.parseDouble(parsedList[17]);
+
+                    leaseContract = new LeaseContract(contractType, contractDate, customerName, customerEmail,
+                            vin, year, make, model, vehicleType, color, mileage, price, expectedEndValue,
+                            leaseFee, totalPrice, monthlyPayment);
+                }
+                else {
+                    System.out.println("Whoops, no leases!");
+                }
+            }
+            bufReader.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return leaseContract;
 
     }
 

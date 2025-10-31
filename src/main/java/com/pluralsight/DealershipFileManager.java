2d3788e34994bc0a72eaacc4774cd42752a44dca
Daniel Spezialetti <dspezialetti@my.yearupunited.org>
2025-10-31 09:54:01 -0400

    * began working on adding the bufWriter to allow the user to save data to the csv file

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,9 +1,6 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
 import java.util.ArrayList;
 
 public class DealershipFileManager {
@@ -49,40 +46,18 @@ public class DealershipFileManager {
     }
 
     public static void saveDealership(Dealership dealership) {
-//        try {
-//            BufferedWriter bufWriter = new BufferedWriter(new FileWriter("src/main/resources/inventory.csv", true));
-//
-//            String input = "";
-//            String[] parsedList = input.split("\\|");
-//
-//            while ((bufWriter.write(dealership)) != null) {
-//
-//                if (parsedList.length > 3) {
-//                    int vin = Integer.parseInt(parsedList[0]);
-//                    int year = Integer.parseInt(parsedList[1]);
-//                    String make = parsedList[2];
-//                    String model = parsedList[3];
-//                    String vehicleType = parsedList[4];
-//                    String color = parsedList[5];
-//                    int odometer = Integer.parseInt(parsedList[6]);
-//                    double price = Double.parseDouble(parsedList[7]);
-//
-//                    inventory.add(new Vehicle(vin, year, make, model, vehicleType, color, odometer, price));
-//                }
-//                else {
-//                    String name = parsedList[0];
-//                    String address = parsedList[1];
-//                    String phoneNum = parsedList[2];
-//
-//                    dealership = new Dealership(name, address, phoneNum);
-//                }
-//
-//            }
-//            bufWriter.close();
-//
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-    }
+        try {
+            BufferedWriter bufWriter = new BufferedWriter(new FileWriter("src/main/resources/inventory.csv", true));
+            bufWriter.write(String.format("%s|%s|%s", dealership.getName(), dealership.getAddress(), dealership.getPhone()));
+            bufWriter.newLine();
 
+            for (Vehicle vehicle : dealership.getAllVehicles()) {
+                bufWriter.write(String.format("%d|%d|%s|%s|%s|%s|%d|%.2f", vehicle.getVin(), vehicle.getYear(), vehicle.getMake(), vehicle.getModel(), vehicle.getVehicleType(), vehicle.getColor(), vehicle.getOdometer(), vehicle.getPrice()));
+            }
+
+            bufWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
